d47b7b20de672330b7637a9d6739260abf1f2c78
alaninnovates <alanlichen1@gmail.com>
2025-09-05 23:42:18 -0700

    Persist state, add clear function

--- lib/state/drill-store.ts
+++ lib/state/drill-store.ts
@@ -1,5 +1,6 @@
 import { createStore } from 'zustand/vanilla';
-import { DotbookEntry, sampleDotbook } from '@/lib/dot/sample';
+import { DotbookEntry } from '@/lib/dot/sample';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 export type DrillState = {
     pages: DotbookEntry[];
@@ -7,7 +8,8 @@ export type DrillState = {
 
 export type DrillActions = {
     addPage: (data: DotbookEntry) => void;
-    modifyPage: (page: number, data: DotbookEntry) => void;
+    modifyPage: (page: number, data: Partial<DotbookEntry>) => void;
+    clearPages: () => void;
 };
 
 export type DrillStore = DrillState & DrillActions;
@@ -23,15 +25,24 @@ export const defaultInitState: DrillState = {
 };
 
 export const createDrillStore = (initState: DrillState = defaultInitState) => {
-    return createStore<DrillStore>()((set) => ({
-        ...initState,
-        addPage: (data: DotbookEntry) =>
-            set((state) => ({ pages: [...state.pages, data] })),
-        modifyPage: (page: number, data: DotbookEntry) =>
-            set((state) => ({
-                pages: state.pages.map((p, index) =>
-                    index === page ? data : p,
-                ),
-            })),
-    }));
+    return createStore<DrillStore>()(
+        persist(
+            (set) => ({
+                ...initState,
+                addPage: (data: DotbookEntry) =>
+                    set((state) => ({ pages: [...state.pages, data] })),
+                modifyPage: (page: number, data: Partial<DotbookEntry>) =>
+                    set((state) => ({
+                        pages: state.pages.map((p, index) =>
+                            index === page ? { ...p, ...data } : p,
+                        ),
+                    })),
+                clearPages: () => set({ pages: [] }),
+            }),
+            {
+                name: 'drill-storage',
+                storage: createJSONStorage(() => localStorage),
+            },
+        ),
+    );
 };
